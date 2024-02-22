0825b6d15d7851fa7a9744a13bd79f98be5bbff4
Rafael Valverde <omit.mv@gmail.com>
2024-02-22 15:17:45 -0300

    Correção na authenticação

--- src/main/java/com/prjapitrans/controller/AuthController.java
+++ src/main/java/com/prjapitrans/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.prjapitrans.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -37,17 +38,25 @@ public class AuthController {
 
   @SuppressWarnings("rawtypes")
   @PostMapping("/auth")
-  public ResponseEntity createAuthenticationToken(@RequestBody LoginRequest authenticationRequest) throws Exception {
-    final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getLogin());
-    LOGGER.info("####### DEBUG 1: " + userDetails.getUsername() + " - " + userDetails.getPassword());
-    Usuario usuario = usuarioService.getByLoginAndSenhaMD5(authenticationRequest.getLogin(),
-        authenticationRequest.getSenha());
-    ObjectMapper json = new ObjectMapper();
-    LOGGER.info("####### DEBUG 3: " + json.writeValueAsString(usuario));
-    final String jwt = jwtUtil.generateToken(json.writeValueAsString(usuario));
-    LOGGER.info("####### DEBUG 4: " + jwt);
-
-    return ResponseEntity.ok(jwt);
+  public ResponseEntity createAuthenticationToken(@RequestBody LoginRequest authenticationRequest) {
+    try {
+      final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getLogin());
+      String jwt = "";
+      LOGGER.info("####### DEBUG 1: " + userDetails.getUsername() + " - " + userDetails.getPassword());
+      Usuario usuario = usuarioService.getByLoginAndSenhaMD5(authenticationRequest.getLogin(),
+          authenticationRequest.getSenha());
+      if (!usuario.equals(null)) {
+        ObjectMapper json = new ObjectMapper();
+        LOGGER.info("####### DEBUG 2: " + json.writeValueAsString(usuario));
+        jwt = jwtUtil.generateToken(json.writeValueAsString(usuario));
+        LOGGER.info("####### DEBUG 3: " + jwt);
+        return ResponseEntity.ok(jwt);
+      } else {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+      }
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
   }
 
 }
